2852255310d7ac22411be4ad141e27ecf6845876
Mike Peiman <mikepeiman@gmail.com>
2018-10-14 10:58:02 -0700

    First commit. Learning basic routing conventions in NuxtJS

---
+++ pages/users/_user_id.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>This is the User ID page. Single user {{ $route.params.user_id }}</h2>
+  <!-- // params.user_id because the file name is _user_id. That provides the key and returns the value -->
+</template>
---
+++ pages/users/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h1>This is the users page</h1>
+    <div>
+      <input type="text" v-model="user_id">
+      <button @click="onLoadUser">Load user</button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user_id: null
+    }
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.user_id)
+    }
+  }
+}
+</script>
